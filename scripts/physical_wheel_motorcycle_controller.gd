c4f5bad97ab10b209d6af9f9bb73ecf795e666ed
dginovker <danielginovker@gmail.com>
2026-06-07 20:11:22 -0700

    better motorbikwe

--- scripts/physical_wheel_motorcycle_controller.gd
+++ scripts/physical_wheel_motorcycle_controller.gd
@@ -8,20 +8,20 @@ const TERRAIN_DIAGONAL_FREQUENCY = 0.18
 const TERRAIN_CROSS_FREQUENCY = 0.13
 const WORLD_COLLIDER_NAME = "GeneratedTrackCollision"
 
-@export var engine_force: float = 420.0
+@export var engine_force: float = 1200.0
 @export var brake_force: float = 680.0
 @export var reverse_force: float = 260.0
-@export var max_forward_speed: float = 34.0
+@export var max_forward_speed: float = 70.0
 @export var max_reverse_speed: float = 10.0
 @export var tire_lateral_grip: float = 1.85
 @export var tire_longitudinal_grip: float = 1.25
 @export var air_drag: float = 1.1
-@export var upright_stability: float = 70.0
-@export var angular_stability_damping: float = 24.0
+@export var upright_stability: float = 180.0
+@export var angular_stability_damping: float = 55.0
 @export var max_front_steer: float = 0.36
-@export var max_lean: float = 0.34
+@export var max_lean: float = 0.523599
 @export var visual_smoothing: float = 10.0
-@export var max_angular_speed: float = 4.0
+@export var max_angular_speed: float = 6.0
 @export var max_vertical_speed: float = 18.0
 @export var wheel_mass: float = 18.0
 @export var rear_wheel_radius: float = 0.60
@@ -32,12 +32,15 @@ const WORLD_COLLIDER_NAME = "GeneratedTrackCollision"
 @export var steering_torque: float = 260.0
 @export var max_drive_torque: float = 950.0
 @export var max_brake_torque: float = 1200.0
-@export var lean_torque: float = 680.0
-@export var lean_turn_torque: float = 430.0
+@export var lean_torque: float = 2100.0
+@export var lean_turn_torque: float = 2300.0
+@export var camera_base_fov: float = 74.0
+@export var camera_speed_fov_boost: float = 60.0
 @export var wheel_friction_multiplier: float = 2.4
 @export var track_friction_multiplier: float = 2.8
 
 var _visual_lean: float = 0.0
+var _front_steer_angle: float = 0.0
 var _raw_forward_pressed: bool = false
 var _raw_reverse_pressed: bool = false
 var _raw_left_pressed: bool = false
@@ -53,6 +56,8 @@ var _handlebar_base_rotation: Vector3
 var _left_grip_base_rotation: Vector3
 var _right_grip_base_rotation: Vector3
 var _headlight_base_rotation: Vector3
+var _front_wheel_tire_visual_rest: Transform3D = Transform3D.IDENTITY
+var _front_wheel_hub_visual_rest: Transform3D = Transform3D.IDENTITY
 var _front_wheel_body: RigidBody3D
 var _rear_wheel_body: RigidBody3D
 var _front_hinge: HingeJoint3D
@@ -132,12 +137,15 @@ func _physics_process(delta: float) -> void:
     var brake_pressed: bool = _is_reverse_pressed()
     var steer_input: float = _get_steer_input()
     _apply_rear_wheel_torque(throttle_pressed, brake_pressed)
-    _apply_lean_steering(steer_input)
-    _apply_balance_assist(steer_input)
+    var reverse_only: bool = brake_pressed and not throttle_pressed and absf(steer_input) <= 0.01
+    _update_front_steering(steer_input)
+    _apply_tire_steering_forces(reverse_only)
+    _apply_balance_assist(steer_input, reverse_only)
     _apply_body_drag()
     _limit_unstable_velocity()
     _update_camera_orbit()
-    _update_visuals(steer_input, delta)
+    _update_camera_fov(delta)
+    _update_visuals(delta)
 
 
 func _configure_frame_body() -> void:
@@ -164,6 +172,7 @@ func _create_physical_wheels() -> void:
         parent_node = self
     _front_wheel_body = _create_wheel_body("FrontPhysicalWheel", _front_wheel_tire, _front_wheel_hub, front_wheel_radius, front_wheel_width, parent_node)
     _rear_wheel_body = _create_wheel_body("RearPhysicalWheel", _rear_wheel_tire, _rear_wheel_hub, rear_wheel_radius, rear_wheel_width, parent_node)
+    _cache_front_wheel_visual_rest_pose()
     _front_hinge = _create_wheel_hinge("FrontWheelHinge", _front_wheel_body, parent_node, false)
     _rear_hinge = _create_wheel_hinge("RearWheelHinge", _rear_wheel_body, parent_node, true)
 
@@ -202,11 +211,16 @@ func _reparent_visual_to_wheel(visual: Node3D, wheel_body: RigidBody3D) -> void:
     visual.global_transform = old_global_transform
 
 
+func _cache_front_wheel_visual_rest_pose() -> void:
+    _front_wheel_tire_visual_rest = _front_wheel_tire.transform
+    _front_wheel_hub_visual_rest = _front_wheel_hub.transform
+
+
 func _create_wheel_hinge(hinge_name: String, wheel_body: RigidBody3D, parent_node: Node, _motor_enabled: bool) -> HingeJoint3D:
     var hinge: HingeJoint3D = HingeJoint3D.new()
     hinge.name = hinge_name
     parent_node.add_child(hinge)
-    hinge.global_transform = _make_hinge_transform(wheel_body)
+    hinge.global_transform = _make_hinge_transform(wheel_body, 0.0, false)
     hinge.node_a = hinge.get_path_to(self)
     hinge.node_b = hinge.get_path_to(wheel_body)
     hinge.exclude_nodes_from_collision = true
@@ -217,14 +231,21 @@ func _create_wheel_hinge(hinge_name: String, wheel_body: RigidBody3D, parent_nod
     return hinge
 
 
-func _make_hinge_transform(wheel_body: RigidBody3D) -> Transform3D:
+func _make_hinge_transform(wheel_body: RigidBody3D, steer_angle: float, use_body_axes: bool) -> Transform3D:
     var axle: Vector3 = wheel_body.global_transform.basis.y.normalized()
     var up_hint: Vector3 = Vector3.UP
+    if use_body_axes:
+        up_hint = global_transform.basis.y.normalized()
+        axle = _get_body_right_on_ground(up_hint).rotated(up_hint, steer_angle).normalized()
     if absf(axle.dot(up_hint)) > 0.95:
-        up_hint = Vector3.FORWARD
+        up_hint = global_transform.basis.z.normalized()
     var x_axis: Vector3 = up_hint.cross(axle).normalized()
     var y_axis: Vector3 = axle.cross(x_axis).normalized()
     return Transform3D(Basis(x_axis, y_axis, axle), wheel_body.global_position)
+
+
+func _update_front_steering(steer_input: float) -> void:
+    _front_steer_angle = -steer_input * max_front_steer
 
 
 func _apply_rear_wheel_torque(throttle_pressed: bool, brake_pressed: bool) -> void:
@@ -244,18 +265,50 @@ func _apply_rear_wheel_torque(throttle_pressed: bool, brake_pressed: bool) -> vo
         _rear_wheel_body.apply_torque(axle * drive_torque)
 
 
-func _apply_lean_steering(steer_input: float) -> void:
+func _apply_tire_steering_forces(reverse_only: bool) -> void:
+    if reverse_only or _front_wheel_body == null or _rear_wheel_body == null:
+        return
     var horizontal_velocity: Vector3 = Vector3(linear_velocity.x, 0.0, linear_velocity.z)
     var speed: float = horizontal_velocity.length()
-    if absf(steer_input) > 0.01:
-        var roll_axis: Vector3 = -global_transform.basis.z.normalized()
-        apply_torque(roll_axis * steer_input * lean_torque)
-    var lean_angle: float = asin(clampf(global_transform.basis.y.normalized().dot(global_transform.basis.x.normalized().cross(Vector3.UP)), -1.0, 1.0))
-    var speed_factor: float = clampf(speed / 12.0, 0.0, 1.0)
-    if absf(lean_angle) > 0.01 and speed > 0.5:
-        apply_torque(Vector3.UP * lean_angle * lean_turn_torque * speed_factor)
-    elif absf(steer_input) > 0.01:
-        apply_torque(Vector3.UP * steer_input * steering_torque * 0.18)
+    if speed < 0.5:
+        return
+    var ground_up: Vector3 = _get_ground_normal(global_position)
+    if ground_up.dot(Vector3.UP) < 0.82:
+        ground_up = Vector3.UP
+    _apply_single_tire_side_force(_front_wheel_body.global_position, front_wheel_radius, _get_steered_front_axle(ground_up), ground_up, 1.0)
+    _apply_single_tire_side_force(_rear_wheel_body.global_position, rear_wheel_radius, _get_body_right_on_ground(ground_up), ground_up, 0.72)
+
+
+func _apply_single_tire_side_force(wheel_position: Vector3, wheel_radius: float, wheel_axle: Vector3, ground_up: Vector3, grip_scale: float) -> void:
+    var contact_point: Vector3 = wheel_position - ground_up * wheel_radius
+    var relative_position: Vector3 = contact_point - global_position
+    var point_velocity: Vector3 = linear_velocity + angular_velocity.cross(relative_position)
+    var lateral_axis: Vector3 = (wheel_axle - ground_up * wheel_axle.dot(ground_up)).normalized()
+    var lateral_speed: float = point_velocity.dot(lateral_axis)
+    var normal_load: float = mass * 9.8 * 0.5 * grip_scale
+    var max_side_force: float = normal_load * tire_lateral_grip
+    var desired_force: Vector3 = -lateral_axis * lateral_speed * lean_turn_torque * grip_scale
+    var side_force: Vector3 = _clamp_vector_length(desired_force, max_side_force)
+    apply_force(side_force, relative_position)
+
+
+func _get_steered_front_axle(ground_up: Vector3) -> Vector3:
+    return _get_body_right_on_ground(ground_up).rotated(ground_up.normalized(), _front_steer_angle).normalized()
+
+
+func _get_body_right_on_ground(ground_up: Vector3) -> Vector3:
+    var body_right: Vector3 = global_transform.basis.x
+    var projected_right: Vector3 = body_right - ground_up * body_right.dot(ground_up)
+    if projected_right.length() < 0.001:
+        projected_right = Vector3.RIGHT
+    return projected_right.normalized()
+
+
+func _clamp_vector_length(vector: Vector3, max_length: float) -> Vector3:
+    var length: float = vector.length()
+    if length > max_length and length > 0.001:
+        return vector * (max_length / length)
+    return vector
 
 
 func _apply_body_drag() -> void:
@@ -278,7 +331,25 @@ func _limit_unstable_velocity() -> void:
 
 
 func _update_camera_orbit() -> void:
-    _camera_rig.rotation = Vector3(_camera_orbit_pitch, _camera_orbit_yaw, 0.0)
+    var rig_anchor: Vector3 = global_position + Vector3.UP * 1.65
+    var body_forward: Vector3 = -global_transform.basis.z
+    var flat_forward: Vector3 = Vector3(body_forward.x, 0.0, body_forward.z)
+    if flat_forward.length() < 0.001:
+        flat_forward = Vector3.FORWARD
+    flat_forward = flat_forward.normalized()
+    flat_forward = flat_forward.rotated(Vector3.UP, _camera_orbit_yaw).normalized()
+    var right_axis: Vector3 = flat_forward.cross(Vector3.UP).normalized()
+    var yaw_basis: Basis = Basis(right_axis, Vector3.UP, -flat_forward)
+    var pitch_basis: Basis = Basis(right_axis, _camera_orbit_pitch)
+    _camera_rig.global_transform = Transform3D(pitch_basis * yaw_basis, rig_anchor)
+
+
+func _update_camera_fov(delta: float) -> void:
+    var horizontal_speed: float = Vector3(linear_velocity.x, 0.0, linear_velocity.z).length()
+    var speed_ratio: float = clampf(horizontal_speed / maxf(max_forward_speed, 0.001), 0.0, 1.0)
+    var target_fov: float = camera_base_fov + camera_speed_fov_boost * speed_ratio
+    var fov_weight: float = clampf(visual_smoothing * delta, 0.0, 1.0)
+    _camera.fov = lerpf(_camera.fov, target_fov, fov_weight)
 
 
 func _create_world_collision() -> void:
@@ -331,7 +402,7 @@ func _create_debug_sliders() -> void:
     panel.offset_left = -330.0
     panel.offset_top = 16.0
     panel.offset_right = -16.0
-    panel.offset_bottom = 178.0
+    panel.offset_bottom = 370.0
     panel.mouse_filter = Control.MOUSE_FILTER_STOP
     canvas.add_child(panel)
     var margin: MarginContainer = MarginContainer.new()
@@ -347,6 +418,11 @@ func _create_debug_sliders() -> void:
     _add_debug_slider(box, "Acceleration", "engine_force", engine_force, 80.0, 1200.0, 10.0)
     _add_debug_slider(box, "Weight", "mass", mass, 60.0, 500.0, 5.0)
     _add_debug_slider(box, "Traction", "tire_lateral_grip", tire_lateral_grip, 0.25, 5.0, 0.05)
+    _add_debug_slider(box, "Max Lean", "max_lean_degrees", rad_to_deg(max_lean), 0.0, 85.0, 1.0)
+    _add_debug_slider(box, "Lean Speed", "lean_torque", lean_torque, 100.0, 20000.0, 100.0)
+    _add_debug_slider(box, "Turn Power", "lean_turn_torque", lean_turn_torque, 0.0, 10000.0, 100.0)
+    _add_debug_slider(box, "Camera FOV", "camera_base_fov", camera_base_fov, 40.0, 110.0, 1.0)
+    _add_debug_slider(box, "Speed FOV", "camera_speed_fov_boost", camera_speed_fov_boost, 0.0, 60.0, 1.0)
 
 
 func _add_debug_slider(parent: VBoxContainer, title: String, property_name: String, current_value: float, minimum: float, maximum: float, step: float) -> void:
@@ -389,6 +465,16 @@ func _on_debug_slider_changed(value: float, property_name: String, value_label: 
             tire_lateral_grip = value
             tire_longitudinal_grip = maxf(value * 0.68, 0.1)
             _update_physics_material_friction()
+        "max_lean_degrees":
+            max_lean = deg_to_rad(clampf(value, 0.0, 85.0))
+        "lean_torque":
+            lean_torque = value
+        "lean_turn_torque":
+            lean_turn_torque = value
+        "camera_base_fov":
+            camera_base_fov = value
+        "camera_speed_fov_boost":
+            camera_speed_fov_boost = value
 
 
 func _format_slider_value(value: float) -> String:
@@ -404,34 +490,84 @@ func _update_physics_material_friction() -> void:
         _track_physics_material.friction = track_friction
 
 
-func _apply_balance_assist(steer_input: float) -> void:
+func _apply_balance_assist(steer_input: float, reverse_only: bool) -> void:
     var horizontal_velocity: Vector3 = Vector3(linear_velocity.x, 0.0, linear_velocity.z)
     var speed_factor: float = clampf(horizontal_velocity.length() / 14.0, 0.0, 1.0)
     var ground_up: Vector3 = _get_ground_normal(global_position)
     if ground_up.dot(Vector3.UP) < 0.82:
         ground_up = Vector3.UP
+    var roll_axis: Vector3 = -global_transform.basis.z.normalized()
+    var target_lean_angle: float = _get_target_lean_angle(steer_input, speed_factor)
+    var current_lean_angle: float = _get_body_lean_angle(ground_up)
+    var lean_error: float = target_lean_angle - current_lean_angle
+    var roll_velocity: float = angular_velocity.dot(roll_axis)
+    var roll_stiffness: float = upright_stability + lean_torque
+    var roll_damping: float = angular_stability_damping * 2.0
+    if reverse_only:
+        roll_stiffness *= 2.0
+        roll_damping *= 1.6
+    apply_torque(roll_axis * (lean_error * roll_stiffness - roll_velocity * roll_damping))
+    _apply_lean_limit(ground_up)
+    var yaw_velocity: float = angular_velocity.dot(Vector3.UP)
+    var non_yaw_angular_velocity: Vector3 = angular_velocity - Vector3.UP * yaw_velocity
+    apply_torque(-non_yaw_angular_velocity * angular_stability_damping * 0.25)
+
+
+func _get_target_lean_angle(steer_input: float, speed_factor: float) -> float:
+    if absf(steer_input) <= 0.01:
+        return 0.0
+    var safe_max_lean: float = _get_safe_max_lean()
+    var low_speed_lean_factor: float = lerpf(0.35, 1.0, speed_factor)
+    return clampf(steer_input * safe_max_lean * low_speed_lean_factor, -safe_max_lean, safe_max_lean)
+
+
+func _get_body_lean_angle(up_axis: Vector3) -> float:
     var forward_axis: Vector3 = -global_transform.basis.z.normalized()
-    var target_lean_angle: float = -steer_input * max_lean * speed_factor
-    var target_up: Vector3 = ground_up.rotated(forward_axis, target_lean_angle).normalized()
+    var ground_up: Vector3 = up_axis.normalized()
     var body_up: Vector3 = global_transform.basis.y.normalized()
-    var correction_axis: Vector3 = body_up.cross(target_up)
-    var correction_strength: float = correction_axis.length()
-    if correction_strength > 0.001:
-        apply_torque(correction_axis.normalized() * correction_strength * upright_stability)
-    apply_torque(-angular_velocity * angular_stability_damping)
+    var signed_sine: float = forward_axis.dot(ground_up.cross(body_up))
+    var cosine: float = clampf(ground_up.dot(body_up), -1.0, 1.0)
+    return atan2(signed_sine, cosine)
 
 
-func _update_visuals(steer_input: float, delta: float) -> void:
+func _apply_lean_limit(up_axis: Vector3) -> void:
+    var lean_angle: float = _get_body_lean_angle(up_axis)
+    var safe_max_lean: float = _get_safe_max_lean()
+    var overshoot: float = absf(lean_angle) - safe_max_lean
+    if overshoot <= 0.0:
+        return
+    var roll_axis: Vector3 = -global_transform.basis.z.normalized()
+    apply_torque(roll_axis * -signf(lean_angle) * overshoot * lean_torque * 3.0)
+
+
+func _get_safe_max_lean() -> float:
+    return minf(absf(max_lean), deg_to_rad(85.0))
+
+
+func _update_visuals(delta: float) -> void:
     var smoothing_weight: float = clampf(visual_smoothing * delta, 0.0, 1.0)
-    var speed_ratio: float = clampf(Vector3(linear_velocity.x, 0.0, linear_velocity.z).length() / max_forward_speed, 0.0, 1.0)
-    _visual_lean = lerpf(_visual_lean, steer_input * max_lean * speed_ratio, smoothing_weight)
-    _model.rotation = _model_base_rotation + Vector3(0.0, 0.0, _visual_lean)
-    _front_fork_left.rotation = _front_fork_left_base_rotation + Vector3(0.0, -steer_input * max_front_steer, 0.0)
-    _front_fork_right.rotation = _front_fork_right_base_rotation + Vector3(0.0, -steer_input * max_front_steer, 0.0)
-    _handlebar.rotation = _handlebar_base_rotation + Vector3(0.0, -steer_input * max_front_steer, 0.0)
-    _left_grip.rotation = _left_grip_base_rotation + Vector3(0.0, -steer_input * max_front_steer, 0.0)
-    _right_grip.rotation = _right_grip_base_rotation + Vector3(0.0, -steer_input * max_front_steer, 0.0)
-    _headlight.rotation = _headlight_base_rotation + Vector3(0.0, -steer_input * max_front_steer, 0.0)
+    _visual_lean = lerpf(_visual_lean, 0.0, smoothing_weight)
+    _model.rotation = _model_base_rotation
+    _front_fork_left.rotation = _front_fork_left_base_rotation + Vector3(0.0, _front_steer_angle, 0.0)
+    _front_fork_right.rotation = _front_fork_right_base_rotation + Vector3(0.0, _front_steer_angle, 0.0)
+    _handlebar.rotation = _handlebar_base_rotation + Vector3(0.0, _front_steer_angle, 0.0)
+    _left_grip.rotation = _left_grip_base_rotation + Vector3(0.0, _front_steer_angle, 0.0)
+    _right_grip.rotation = _right_grip_base_rotation + Vector3(0.0, _front_steer_angle, 0.0)
+    _headlight.rotation = _headlight_base_rotation + Vector3(0.0, _front_steer_angle, 0.0)
+    _update_front_wheel_visual_steering()
+
+
+func _update_front_wheel_visual_steering() -> void:
+    if _front_wheel_body == null:
+        return
+    var ground_up: Vector3 = _get_ground_normal(_front_wheel_body.global_position)
+    if ground_up.dot(Vector3.UP) < 0.82:
+        ground_up = Vector3.UP
+    ground_up = ground_up.normalized()
+    var steered_basis: Basis = Basis(ground_up, _front_steer_angle) * _front_wheel_body.global_transform.basis
+    var steered_wheel_transform: Transform3D = Transform3D(steered_basis, _front_wheel_body.global_position)
+    _front_wheel_tire.global_transform = steered_wheel_transform * _front_wheel_tire_visual_rest
+    _front_wheel_hub.global_transform = steered_wheel_transform * _front_wheel_hub_visual_rest
 
 
 func _cache_visual_rest_pose() -> void:
